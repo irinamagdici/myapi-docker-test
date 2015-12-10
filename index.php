53aa04e71265c0f2c3191aab8301ead9071bc2d5
Irina M <irina@xivic.com>
2015-12-11 00:14:40 +0200

    add php index

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Test APP</title>
+</head>
+<body>
+<h1>Test App</h1>
+Bine ati venit in comunitatea de DevOps [Iasi, RO]! 
+<?php
+echo phpinfo();
+?>
+</body>
+</html>
